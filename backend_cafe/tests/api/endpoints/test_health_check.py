209dd3fd2b203a380d3332d997ac0ff6bf2501c1
Siarhei Samoshyn <gds.grey@gmail.com>
2025-04-01 19:33:05 +0300

    add CI for tests and add main test

---
+++ backend_cafe/tests/api/endpoints/test_health_check.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+from backend_cafe.app.main import main_app
+
+client = TestClient(main_app)
+
+
+def test_root():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"message": "application is running"}
