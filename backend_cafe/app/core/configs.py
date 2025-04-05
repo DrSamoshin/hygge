b72ef857ec111fb039060a8f26e6a9d9cba6b6c5
Siarhei Samoshyn <gds.grey@gmail.com>
2025-04-05 15:38:28 +0300

    add logging

--- backend_cafe/app/core/configs.py
+++ backend_cafe/app/core/configs.py
@@ -1,3 +1,4 @@
+import logging
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 
@@ -6,7 +7,15 @@ class Run(BaseModel):
     host: str = "127.0.0.1"
     port: int = 8000
 
+class Logging(BaseModel):
+    logging.basicConfig(
+        level= logging.INFO,
+        format="%(levelname)-9s %(asctime)s - %(module)-15s - %(message)s",
+    )
+
+
 class Settings(BaseSettings):
     run: Run = Run()
+    logging: Logging = Logging()
 
 settings = Settings()
--- backend_cafe/run.py
+++ backend_cafe/run.py
@@ -1,9 +1,12 @@
+import logging
+
 import uvicorn
 from backend_cafe.app.core.configs import settings
 
 
 if __name__ == "__main__":
     try:
+        logging.info("Starting FastAPI")
         uvicorn.run("app.main:main_app", host=settings.run.host, port=settings.run.port, reload=True)
     except Exception as error:
         print(error)
